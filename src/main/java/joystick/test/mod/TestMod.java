b3609d7c2cb99e30cfeb85a1790f192cb1239afe
Joystick299 <bubbalou112@gmail.com>
2021-08-11 19:07:05 -0400

    v1.0.1

--- src/main/java/joystick/test/mod/TestMod.java
+++ src/main/java/joystick/test/mod/TestMod.java
@@ -1,17 +1,33 @@
 package joystick.test.mod;
 
 
+import java.io.ObjectInputFilter.Config;
+
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
+import net.minecraft.client.gl.Uniform;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.feature.Feature;
 
 public class TestMod implements ModInitializer {
 
@@ -21,19 +37,27 @@ public class TestMod implements ModInitializer {
 
 	public static final Item NOTHING_FOOD = new Item(new Item.Settings().group(ItemGroup.FOOD).food(FoodComponents.NOTHING_FOOD));
 
+	public static final Block NOTHING_ORE = new NothingOre(FabricBlockSettings.of(Material.STONE).strength(3.0F, 3.0F).sounds(BlockSoundGroup.ANVIL).breakByTool(FabricToolTags.PICKAXES).requiresTool());
+
+	private static ConfiguredFeature<?, ?> NOTHING_ORE_OVERWORLD = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, NOTHING_ORE.getDefaultState(),9)).range(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.aboveBottom(0), YOffset.fixed(120)))).spreadHorizontally().repeat(20);
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-	Registry.register(Registry.ITEM, new Identifier("joysticktestmod", "nothing_item"), NOTHING_ITEM);
+		Registry.register(Registry.ITEM, new Identifier("joysticktestmod", "nothing_item"), NOTHING_ITEM);
 
-	Registry.register(Registry.BLOCK, new Identifier("joysticktestmod", "nothing_block"), NOTHING_BLOCK);
-	Registry.register(Registry.ITEM, new Identifier("joysticktestmod", "nothing_block"), new BlockItem(NOTHING_BLOCK, new Item.Settings().group(ItemGroup.DECORATIONS)));
+		Registry.register(Registry.BLOCK, new Identifier("joysticktestmod", "nothing_block"), NOTHING_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier("joysticktestmod", "nothing_block"), new BlockItem(NOTHING_BLOCK, new Item.Settings().group(ItemGroup.DECORATIONS)));
 
 		Registry.register(Registry.ITEM, new Identifier("joysticktestmod", "nothing_food"), NOTHING_FOOD);
 		
+
+		Registry.register(Registry.BLOCK, new Identifier("joysticktestmod", "nothing_ore"), NOTHING_ORE);
+		Registry.register(Registry.ITEM, new Identifier("joysticktestmod", "nothing_ore"), new BlockItem(NOTHING_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+
+		RegistryKey<ConfiguredFeature<?, ?>> nothingOreOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier("joysticktestmod", "nothing_ore"));
+		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, nothingOreOverworld.getValue(), NOTHING_ORE_OVERWORLD);
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, nothingOreOverworld);
 
 	}
 }
